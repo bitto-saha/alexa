8ff45c677573c7d9b2ed5fea48f6d3853804b8f2
bitto <79255327+BittoSahaTrinoy@users.noreply.github.com>
2021-06-13 15:55:30 +0600

    Update main.js

--- main.js
+++ main.js
@@ -677,5 +677,54 @@ recognition.onresult = function(event) {
 
 
 }
+//talking tom
+function talktom(){
+
+    const btn = document.getElementById("btn");
+const content = document.getElementById("content");
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition ;
+
+const recognition =  new SpeechRecognition();
+
+
+recognition.onstart = function(){
+  
+  console.log("voic recognition started successfully! speak on your microphoneee!!!");
+}
+
+recognition.onresult = function(event){
+  
+  const current = event.resultIndex;
+  
+  const transcript = event.results[current][0].transcript;
+  content.textContent = transcript;
+  read(transcript);
+  
+}
+
+
+btn.addEventListener("click", ()=> {
+  
+  recognition.start();
+  
+  
+});
+
+
+ function read(message){
+  
+  
+  const speech = new SpeechSynthesisUtterance();
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  speech.text = message;
+  
+  window.speechSynthesis.speak(speech);
+  
+  
+}
+}
+
 
 
